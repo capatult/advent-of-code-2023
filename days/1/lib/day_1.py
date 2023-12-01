4cdf5bcdb7e5f6be8949b768fd699b463b5d53e3
Perran Thomas <perran.thomas@icloud.com>
2023-12-01 12:51:40 +0000

    Wrote parse_document function via TDD

--- days/1/lib/day_1.py
+++ days/1/lib/day_1.py
@@ -2,3 +2,6 @@
 def parse_single_line(line):
     digit_characters = [c for c in line if c.isdigit()]
     return (10 * int(digit_characters[0])) + int(digit_characters[-1])
+
+def parse_document(document):
+    return sum(parse_single_line(line) for line in document.split("\n"))
